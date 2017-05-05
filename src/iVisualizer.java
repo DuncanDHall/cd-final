598d5b0085a102d6b38472cbb42583622665c851
Duncan Hall <duncan.hall@students.olin.edu>
2017-05-05 16:29:08 -0400

    removed legacy classes

--- src/iVisualizer.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by duncan on 4/28/17.
- */
-public interface iVisualizer {
-    public void pulseNode(String nodeID, String options);
-    public void pulseConnection(String senderID, String receiverID, String options);
-}
